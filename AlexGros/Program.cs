ee4ffdffb753f5842504bbc285e5a74e520aaaac
Alex Daniel Gros <alexdgros@yahoo.com>
2020-10-08 19:19:46 +0300

    Am adaugat mystage

---
+++ AlexGros/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlexGros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
